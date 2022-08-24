236763b049dde041df354b387c7d191c2febe192
mounishRavikoti-devops <76801050+mounishRavikoti-devops@users.noreply.github.com>
2022-08-24 07:14:52 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,11 +22,10 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Organization", "XTREAM Technologies");
+		js.put("Trainer", "Mounish Ravikoti");
+		js.put("Stream", "DEVOPS");
+		js.put("good At", "Infrastructure Autmation");
 
 		return js.toString();
 }
